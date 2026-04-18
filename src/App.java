8a234c48deef846f8aedb8307d14d6f2b817e950
DanRin9 <da.rinconr12@uniandes.edu.co>
2026-04-17 19:41:49 -0500

    prueba en main de la parte 1

--- src/App.java
+++ src/App.java
@@ -1,8 +1,46 @@
+import java.util.ArrayList;
+
 public class App {
     
+    private int cantidadSensores;
+    private int numeroEventosBase;
+    private int capacidadBuzonClasificacion;
+
+    public App(int cantidadSensores, int numeroEventos ){
+        this.cantidadSensores = cantidadSensores;
+        this.numeroEventosBase = numeroEventos;
+        
+    }
+
+    public int generarCantidadEventos(int numeroBase, int idSensor){
+        return numeroBase*idSensor;
+    }
     
     public static void main(String[] args) throws Exception {
-        App principal = new App();
+        App mainApp = new App(3,5);
+
+        MonitorEntradaEventos monitorEntrada = new MonitorEntradaEventos();
+
+
+        //CREAR SENSORES
+        SensorIoT[] sensores = new SensorIoT[mainApp.cantidadSensores];
+        int cantidadEventosTotales = 0;
+        for (int i=0;i<mainApp.cantidadSensores;i++){
+            int cantidad = mainApp.generarCantidadEventos(mainApp.numeroEventosBase, i);
+            cantidadEventosTotales += cantidad;
+            sensores[i] = new SensorIoT(cantidad, i+1, 1, monitorEntrada);
+        }
+
+        BrokerAnalizador analizador = new BrokerAnalizador(monitorEntrada, cantidadEventosTotales);
+
+        //INICIAR THREADS
+        analizador.start();
+        for (int i=0;i<mainApp.cantidadSensores;i++){
+            sensores[i].start();
+        }
+        
+
+
         System.out.println("Hello, World!");
     }
 }
